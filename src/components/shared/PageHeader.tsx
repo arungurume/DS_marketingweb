5d640d6c0c738344f2733e9e9b6f9d67cd74746a
Arun <agdigitalsignage@gmail.com>
2025-12-21 19:02:36 +0000

    we can have double tone but defautl shuld be white background

--- src/components/shared/PageHeader.tsx
+++ src/components/shared/PageHeader.tsx
@@ -5,7 +5,7 @@ type PageHeaderProps = {
 
 export function PageHeader({ title, subtitle }: PageHeaderProps) {
   return (
-    <section className="py-16 sm:py-24 bg-secondary">
+    <section className="py-16 sm:py-24 bg-background">
       <div className="container text-center">
         <h1 className="font-headline text-4xl sm:text-5xl font-bold">
           {title}
