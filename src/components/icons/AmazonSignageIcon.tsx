c3813201ddb431a3cd1de55e6aa758feda61d5c1
Arun <agdigitalsignage@gmail.com>
2025-12-22 07:46:33 +0000

    were r the logos for theose players its nto clear

--- src/components/icons/AmazonSignageIcon.tsx
+++ src/components/icons/AmazonSignageIcon.tsx
@@ -9,7 +9,7 @@ export const AmazonSignageIcon = (props: SVGProps<SVGSVGElement>) => (
         {...props}
     >
         <path
-            fill="currentColor"
+            fill="#FF9900"
             d="M20.42 12.89a.5.5 0 0 0 .58-.42a9 9 0 0 0-17.11-1.33a.5.5 0 0 0 .6.42a8 8 0 0 1 15.33 1.33Zm-15-2.2A.5.5 0 0 1 5 10.27a4 4 0 0 1 8 0a.5.5 0 1 1-1 0a3 3 0 0 0-6 0a.5.5 0 0 1-.52.42ZM22 13.5a.5.5 0 0 1-.5.5h-19a.5.5 0 0 1 0-1h19a.5.5 0 0 1 .5.5Z"
         />
     </svg>
--- src/components/icons/WebPlayerIcon.tsx
+++ src/components/icons/WebPlayerIcon.tsx
@@ -6,7 +6,7 @@ export const WebPlayerIcon = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke="#4285F4"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
