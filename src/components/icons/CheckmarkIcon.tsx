f66b09c830d0e76765441afb58af59092e017062
Arun <agdigitalsignage@gmail.com>
2025-12-23 08:42:16 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/icons/CheckmarkIcon.tsx
+++ src/components/icons/CheckmarkIcon.tsx
@@ -2,10 +2,9 @@
 import { cn } from '@/lib/utils';
 import { Check, X } from 'lucide-react';
 
-export const CheckmarkIcon = ({ val }: { val: boolean }) => (
-    val ? 
-    <Check className="h-5 w-5 text-green-500" /> :
-    <X className="h-5 w-5 text-red-500" />
-);
-
-    
+export const CheckmarkIcon = ({ val }: { val: boolean | null | undefined }) => {
+    if (val === true) {
+        return <Check className="h-5 w-5 text-green-500" />;
+    }
+    return <X className="h-5 w-5 text-red-500" />;
+};
