0b9ae0dcd1aefb0635f3cd57bb5daed5a184172a
Arun <agdigitalsignage@gmail.com>
2025-12-30 10:21:25 +0000

    Enable Next.js static export for Firebase Hosting

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,9 @@ const nextConfig = {
     unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-    ],
-  },
+      { protocol: "https", hostname: "images.unsplash.com" }
+    ]
+  }
 };
 
 export default nextConfig;
